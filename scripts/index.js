9fe95758373e692348d53a9b3844390eba4f0e3b
Shecho <shechovala@gmail.com>
2020-02-21 10:32:53 -0500

    validacion agragada

--- scripts/index.js
+++ scripts/index.js
@@ -23,7 +23,23 @@
 
 /* mesaje de enviado del form */
 const enviar = () => {
+  console.log("workingForm");
+  let nombre = document.getElementById("name").value  
+  let correo = document.getElementById("email").value  
+  let mensaje = document.getElementById("message-text").value  
+
+if(!nombre|| !correo|| !mensaje){
+  mostrartexto= document.getElementById("pintarMensaje").innerHTML=`
+ <div class="alert alert-danger alert-dismissible fade show" role="alert">
+    Formulario incompleto, por favor llena todos los campos
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+
+  `
   
+}
+else{
   mostrartexto= document.getElementById("pintarMensaje").innerHTML=`
    <div class="alert alert-success alert-dismissible fade show" role="alert">
   Mensaje enviado
@@ -31,7 +47,7 @@ const enviar = () => {
     <span aria-hidden="true">&times;</span>
   </button> 
     `
-  
+ } 
 }
 
 let send= document.getElementById('send-button').addEventListener("click",enviar)
