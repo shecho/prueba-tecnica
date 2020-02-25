4d3ef23693d627d63195bf18b1af506ac024af43
Shecho <shechovala@gmail.com>
2020-02-24 20:20:40 -0500

    small server added

--- scripts/map.js
+++ scripts/map.js
@@ -1,13 +1,14 @@
 
   function initMap() {
+    console.log("test!");
     // The location of Uluru
-    let uluru = { lat: -25.344, lng: 131.036 };
+    var uluru = { lat: -25.344, lng: 131.036 };
     // The map, centered at Uluru
-    const mapa = new google.maps.Map(document.getElementById('map'), {
+    var mapa = new google.maps.Map(document.getElementById('map'), {
       zoom: 4,
       center: uluru
     });
-    
+    console.log("test2");
   }
 // initMap(mapa)
  
